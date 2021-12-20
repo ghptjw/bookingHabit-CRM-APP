e2074bd9429a44f69932b908bbe15a9b5939c199
homelala <homealal@naver.com>
2021-12-20 17:45:40 +0900

    add: setting Projects

---
+++ server/todayHabitApp/todayHabitApp/src/main/java/todayHabit/todayHabitApp/TodayHabitAppApplication.java
@@ -0,0 +1,13 @@
+package todayHabit.todayHabitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayHabitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayHabitAppApplication.class, args);
+	}
+
+}
---
+++ server/todayHabitApp/todayHabitApp/src/test/java/todayHabit/todayHabitApp/TodayHabitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package todayHabit.todayHabitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHabitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
