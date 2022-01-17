77815b5d0d4e83f2a040fbd37b3590bfaffec37d
yoon-jisung <hmsj2380@gmial.com>
2022-01-17 16:57:29 +0800

    git test

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,12 +4,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: AuthPage()
-    );
-        
+    return MaterialApp(home: AuthPage());
   }
 }
+ // git test
